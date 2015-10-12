a6a7b33ba7a308b701104d692450bfa96ffa7665
Kevin <dragn194@gmail.com>
2015-10-12 10:34:31 -0400

    Add some test code to see if this whole modding thing is actually going to work.

---
+++ SingleUseDockingPorts/SingleUseDockingPorts/ModuleSingleUseDockingPort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SingleUseDockingPorts
+{
+    public class ModuleSingleUseDockingPort : ModuleDockingNode
+    {
+        public override void OnAwake()
+        {
+            print("Initializing ModuleSingleUseDockingPort");
+        }
+
+    }
+}
