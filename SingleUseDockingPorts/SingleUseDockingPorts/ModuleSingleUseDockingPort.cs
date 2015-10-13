0f614d2b68c7b53e0df858c3b1c564497ed37494
Kevin <dragn194@gmail.com>
2015-10-12 21:57:10 -0400

    Add an event handler to the onPartCoupled event that strengthens the joint between the two docked parts.

--- SingleUseDockingPorts/SingleUseDockingPorts/ModuleSingleUseDockingPort.cs
+++ SingleUseDockingPorts/SingleUseDockingPorts/ModuleSingleUseDockingPort.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace SingleUseDockingPorts
 {
     public class ModuleSingleUseDockingPort : ModuleDockingNode
     {
+
         public override void OnAwake()
         {
             print("Initializing ModuleSingleUseDockingPort");
+            //initialize event handler
+            GameEvents.onPartCouple.Add(onPartCoupled);
+        }
+
+        public void OnDestroy()
+        {
+            //deregister event handlers
+            GameEvents.onPartCouple.Remove(onPartCoupled);
         }
 
         public new void Undock()
         {
-            ScreenMessages.PostScreenMessage("This port is single-use only and cannot be decoupled", 2f, ScreenMessageStyle.UPPER_CENTER);
+            ScreenMessages.PostScreenMessage("This port is single-use only and cannot be undocked", 2f, ScreenMessageStyle.UPPER_CENTER);
         }
 
         public new void Decouple()
@@ -22,5 +32,15 @@ namespace SingleUseDockingPorts
             ScreenMessages.PostScreenMessage("This port is single-use only and cannot be decoupled.", 2f, ScreenMessageStyle.UPPER_CENTER);
         }
 
+        private void onPartCoupled(GameEvents.FromToAction<Part, Part> data)
+        {
+            if (data.from == part)
+            {
+                //create a new joint between to the two port's parents supplement the existing one?
+                part.parent.gameObject.AddComponent<FixedJoint>();
+                FixedJoint joint = part.parent.gameObject.GetComponent<FixedJoint>();
+                joint.connectedBody = data.to.parent.gameObject.rigidbody;
+            }
+        }
     }
 }
