8a340d6893e224080989a13eedc07f0d317a8c65
Kevin <dragn194@gmail.com>
2015-10-17 19:59:20 -0400

    Implement the isDockedToPort and isCoupledToPort methods.

--- SingleUseDockingPorts/SingleUseDockingPorts/ModuleSingleUseDockingPort.cs
+++ SingleUseDockingPorts/SingleUseDockingPorts/ModuleSingleUseDockingPort.cs
@@ -10,6 +10,7 @@ namespace SingleUseDockingPorts
     {
         bool clamped = false;
 
+
         Part ghostConnectionFrom = null;
         Part ghostConnectionTo = null;
 
@@ -23,7 +24,14 @@ namespace SingleUseDockingPorts
         [KSPEvent(guiName = "Clamp", externalToEVAOnly = true)]
         public void clamp()
         {
-            
+            if (this.isDockedToPort() != null)
+            {
+                print("This port is docked to " + this.isDockedToPort().name);
+            }
+            if (this.isCoupledToPort() != null)
+            {
+                print("This port is coupled to " + this.isCoupledToPort().name);
+            }
         }
 
         public override void OnFixedUpdate()
@@ -39,16 +47,25 @@ namespace SingleUseDockingPorts
             }
         }
 
-        //checks if this port is coupled to a port, i.e, it's had a port attached in the VAB
+        //checks if this port is coupled to a port, i.e, it's had a port attached in the VAB. Returns the docked part or null if there isn't one.
         private Part isCoupledToPort()
         {
-
+            List<ModuleClampableDockingPort> ports = this.vessel.FindPartModulesImplementing<ModuleClampableDockingPort>();
+            foreach (ModuleClampableDockingPort port in ports)
+            {
+                //if we are this part's parent, or vice versa
+                if (this.part.parent == port.part || port.part.parent == this.part)
+                {
+                    return port.part;
+                }
+            }
+            return null;
         }
 
-        //checks if this port has been docked to a port in the usual way
+        //checks if this port has been docked to a port in the usual way. Returns the docked part or null if there isn't one.
         private Part isDockedToPort()
         {
-
+            return vessel[dockedPartUId];
         }
 
         public new void Undock()
