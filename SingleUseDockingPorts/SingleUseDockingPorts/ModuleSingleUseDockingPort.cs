377fb24d2993936b5044159d450397c8be0e9747
Kevin <dragn194@gmail.com>
2015-10-17 19:40:26 -0400

    Add stubs for a clamp method to clamp two ports together more, and added some convenience methods to see if the port is docked and/or coupled to another port.

--- SingleUseDockingPorts/SingleUseDockingPorts/ModuleSingleUseDockingPort.cs
+++ SingleUseDockingPorts/SingleUseDockingPorts/ModuleSingleUseDockingPort.cs
@@ -6,43 +6,24 @@ using UnityEngine;
 
 namespace SingleUseDockingPorts
 {
-    public class ModuleSingleUseDockingPort : ModuleDockingNode
+    public class ModuleClampableDockingPort : ModuleDockingNode
     {
+        bool clamped = false;
+
         Part ghostConnectionFrom = null;
         Part ghostConnectionTo = null;
+
+        FixedJoint clampJoint;
 
         public override void OnAwake()
         {
             base.OnAwake();
-            print("Initializing ModuleSingleUseDockingPort");
-            //initialize event handler
-            GameEvents.onPartCouple.Add(onPartCoupled);
         }
 
-        public override void OnStart(StartState st)
+        [KSPEvent(guiName = "Clamp", externalToEVAOnly = true)]
+        public void clamp()
         {
-            base.OnStart(st);
-            foreach (Part child in part.children)
-            {
-                print("hunting for children");
-                //look through all the modules on the part
-                foreach (PartModule module in child.Modules)
-                {
-                    //if the module is a single use port
-                    if (module is ModuleSingleUseDockingPort)
-                    {
-                        //cast the module to the proper type
-                        ModuleSingleUseDockingPort singlePort = module as ModuleSingleUseDockingPort;
-                        //if we have the same node type
-                        if (singlePort.nodeType.Equals(this.nodeType))
-                        {
-                            //set the needed connection parts
-                            ghostConnectionFrom = part.parent;
-                            ghostConnectionTo = child.children[0];
-                        }
-                    }
-                }
-            }
+            
         }
 
         public override void OnFixedUpdate()
@@ -50,35 +31,47 @@ namespace SingleUseDockingPorts
             base.OnFixedUpdate();
             if (ghostConnectionFrom != null && ghostConnectionTo != null)
             {
-                FixedJoint joint = ghostConnectionFrom.gameObject.AddComponent<FixedJoint>();
-                joint.connectedBody = ghostConnectionTo.gameObject.rigidbody;
+                print("Making connection between " + ghostConnectionFrom.name + " and " + ghostConnectionTo.name);
+                clampJoint = ghostConnectionFrom.gameObject.AddComponent<FixedJoint>();
+                clampJoint.connectedBody = ghostConnectionTo.gameObject.rigidbody;
                 ghostConnectionFrom = null;
                 ghostConnectionTo = null;
             }
         }
 
-        public void OnDestroy()
+        //checks if this port is coupled to a port, i.e, it's had a port attached in the VAB
+        private Part isCoupledToPort()
         {
-            //deregister event handlers
-            GameEvents.onPartCouple.Remove(onPartCoupled);
+
+        }
+
+        //checks if this port has been docked to a port in the usual way
+        private Part isDockedToPort()
+        {
+
         }
 
         public new void Undock()
         {
-            ScreenMessages.PostScreenMessage("This port is single-use only and cannot be undocked", 2f, ScreenMessageStyle.UPPER_CENTER);
+            if (clamped)
+            {
+                ScreenMessages.PostScreenMessage("This port is clamped and cannot be undocked", 2f, ScreenMessageStyle.UPPER_CENTER);
+            }
+            else
+            {
+                base.Undock();
+            }
         }
 
         public new void Decouple()
         {
-            ScreenMessages.PostScreenMessage("This port is single-use only and cannot be decoupled.", 2f, ScreenMessageStyle.UPPER_CENTER);
-        }
-
-        private void onPartCoupled(GameEvents.FromToAction<Part, Part> data)
-        {
-            if (data.from == part)
+            if (clamped)
             {
-                ghostConnectionFrom = part.parent;
-                ghostConnectionTo = data.to.parent;
+                ScreenMessages.PostScreenMessage("This port is clamped and cannot be decoupled.", 2f, ScreenMessageStyle.UPPER_CENTER);
+            }
+            else
+            {
+                base.Decouple();
             }
         }
     }
